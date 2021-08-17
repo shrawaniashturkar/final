82c3babdf795ede1cc39ba792c9236ee47fe3a96
ADMIN <ADMIN@DESKTOP-RORU9LI>
2021-08-17 10:15:03 +0530

    de

--- src/finalgitdemo/animal.java
+++ src/finalgitdemo/animal.java
@@ -4,6 +4,8 @@ public class animal {
 
 	public static void main(String[] args) {
 		System.out.println("what to do now");
+		System.out.println("Hello guys");
+		System.out.println("I am fine");
 
 	}
 
